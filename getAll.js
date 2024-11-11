63e6ee31644fbf540d8964274e7526d6fc3f1a91
Matteo Denni <matteo.denni@gmail.com>
2024-11-11 17:08:34 +0100

    new function to download all images

--- getAll.js
+++ getAll.js
@@ -1,7 +1,1 @@
-"use strict"
-document.addEventListener('DOMContentLoaded', function() {
-    var alertButton = document.getElementById('alertButton');
-    alertButton.addEventListener('click', function() {
-      alert('Hello from the Chrome extension!');
-    });
-  });
+
